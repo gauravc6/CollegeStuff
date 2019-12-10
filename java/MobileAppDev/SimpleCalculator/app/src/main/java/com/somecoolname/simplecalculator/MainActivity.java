48a87379d1f0e1b56478007a9149edf7ead3dd5d
Gaurav Chandiramani <47972788+gauravc6@users.noreply.github.com>
2019-12-10 13:54:21 +0530

    Add working backend

--- java/MobileAppDev/SimpleCalculator/app/src/main/java/com/somecoolname/simplecalculator/MainActivity.java
+++ java/MobileAppDev/SimpleCalculator/app/src/main/java/com/somecoolname/simplecalculator/MainActivity.java
@@ -4,11 +4,80 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText field1;
+    private EditText field2;
+    private TextView resText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_relative);
+    }
+
+    public void addNum(View v){
+        field1 = findViewById(R.id.editText);
+        field2 = findViewById(R.id.editText2);
+        float num1 = Float.valueOf(field1.getText().toString());
+        float num2 = Float.valueOf(field2.getText().toString());
+        float res = num1 + num2;
+        setVal(res);
+    }
+    
+    public void subNum(View v){
+        field1 = findViewById(R.id.editText);
+        field2 = findViewById(R.id.editText2);
+        float num1 = Float.valueOf(field1.getText().toString());
+        float num2 = Float.valueOf(field2.getText().toString());
+        float res = num1 - num2;
+        setVal(res);
+    }
+
+    public void multNum(View v){
+        field1 = findViewById(R.id.editText);
+        field2 = findViewById(R.id.editText2);
+        float num1 = Float.valueOf(field1.getText().toString());
+        float num2 = Float.valueOf(field2.getText().toString());
+        float res = num1 * num2;
+        setVal(res);
+    }
+
+    public void divNum(View v){
+        field1 = findViewById(R.id.editText);
+        field2 = findViewById(R.id.editText2);
+        float num1 = Float.valueOf(field1.getText().toString());
+        float num2 = Float.valueOf(field2.getText().toString());
+        float res = num1 / num2;
+        setVal(res);
+    }
+
+    public void modNum(View v){
+        field1 = findViewById(R.id.editText);
+        field2 = findViewById(R.id.editText2);
+        float num1 = Float.valueOf(field1.getText().toString());
+        float num2 = Float.valueOf(field2.getText().toString());
+        float res = num1 % num2;
+        setVal(res);
+    }
+
+    public void resetField(View v){
+        field1 = findViewById(R.id.editText);
+        field2 = findViewById(R.id.editText2);
+        field1.setText(String.valueOf(0));
+        field2.setText(String.valueOf(0));
+        setVal(0);
+    }
+
+    public void setVal(float x){
+        resText = findViewById(R.id.resText);
+        resText.setText("Result = "+x);
+
     }
 }
