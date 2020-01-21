83058ad221a01fc2dab5f6ac256013be6e95bd9f
gauravc6 <gauravchandiramani6@gmail.com>
2020-01-21 14:06:07 +0530

    add RatingApp

---
+++ java/MobileAppDev/RatingApp/app/src/main/java/com/somecoolname/ratingapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.somecoolname.ratingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    RatingBar bar;
+    TextView ratingView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bar = findViewById(R.id.ratingBar);
+        ratingView = findViewById(R.id.rateView);
+        bar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                ratingView.setText("Rating: "+ rating);
+            }
+        });
+    }
+}
