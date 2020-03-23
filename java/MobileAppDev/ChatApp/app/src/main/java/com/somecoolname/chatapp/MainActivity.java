5a72c24da5d34de4edb2256d3edee626822d1668
Gaurav Chandiramani <47972788+gauravc6@users.noreply.github.com>
2020-03-23 11:23:45 +0530

    finish ChatApp

--- java/MobileAppDev/ChatApp/app/src/main/java/com/somecoolname/chatapp/MainActivity.java
+++ java/MobileAppDev/ChatApp/app/src/main/java/com/somecoolname/chatapp/MainActivity.java
@@ -1,13 +1,25 @@
 package com.somecoolname.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import com.firebase.client.Firebase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
 
         final EditText userMessage;
         ImageView sendButton;
+        final ListView listView;
+        final ArrayAdapter<String> adapter;
+        final ArrayList<String> chatList;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -32,6 +47,28 @@ public class MainActivity extends AppCompatActivity {
                 Chat chat = new Chat("Gaurav",userMessage.getText().toString());
                 ref.push().setValue(chat);
                 userMessage.setText("");
+            }
+        });
+
+        listView = findViewById(R.id.chatListView);
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference mref = firebaseDatabase.getReference("chat");
+        chatList = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(this, R.layout.list_layout, R.id.textView, chatList);
+        mref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                chatList.clear();
+                for (DataSnapshot ds: dataSnapshot.getChildren()) {
+                    Chat chat = ds.getValue(Chat.class);
+                    chatList.add(chat.getUsername()+ ":\n" + " " + chat.getMessage());
+                }
+                listView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
     }
